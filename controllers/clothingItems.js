c933067b841966713d5374f68bf885150260b027
Donovan Jabbar <djabbar51@gmail.com>
2025-07-06 16:09:49 -0600

    feat: implement controllers for items routes
    - implement /GET, /POST & /DELETE controllers for /items route

---
+++ controllers/clothingItems.js
@@ -0,0 +1,60 @@
+const { ERROR_CODES, sendErrorCode } = require("../utils/errors");
+const Item = require("../models/clothingItem");
+
+const getAllItems = async (req, res) => {
+  try {
+    const items = await Item.find({}).populate("owner").populate("likes");
+    res.status(200).json({ items });
+  } catch (error) {
+    sendErrorCode(req, res, error);
+  }
+};
+
+const addNewItem = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    if (!userId) {
+      res
+        .status(ERROR_CODES.BAD_REQUEST)
+        .json({ message: "Missing _id property" });
+      return;
+    }
+
+    const { name, weather, imageUrl } = req.body;
+    if (!name || !weather || !imageUrl) {
+      res
+        .status(ERROR_CODES.BAD_REQUEST)
+        .json({ message: "Missing required field/s" });
+      return;
+    }
+
+    const item = await Item.create({ name, weather, imageUrl, owner: userId });
+    await item.populate("owner");
+    res.status(201).json(item);
+  } catch (error) {
+    sendErrorCode(req, res, error);
+  }
+};
+
+const deleteItem = async (req, res) => {
+  try {
+    const { itemId } = req.params;
+    if (!itemId) {
+      res
+        .status(ERROR_CODES.BAD_REQUEST)
+        .json({ message: "Missing required parameter: itemId" });
+      return;
+    }
+
+    const deletedItem = await Item.findByIdAndDelete(itemId).orFail();
+    res.status(200).json({message: "Deleted successfully", item: deletedItem})
+  } catch (error) {
+    sendErrorCode(req, res, error);
+  }
+};
+
+module.exports = {
+  getAllItems,
+  addNewItem,
+  deleteItem,
+};
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
+const { getAllItems, addNewItem, deleteItem } = require("../controllers/clothingItems")
+
+router.route("/")
+  .get(getAllItems)
+  .post(addNewItem)
+
+router.route('/:itemId')
+  .delete(deleteItem)
 
 module.exports = router
