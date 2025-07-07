678f48a6da0771c781b77f67887492678d71e22e
Donovan Jabbar <djabbar51@gmail.com>
2025-07-07 00:15:28 -0600

    refactor: replace hard-coded status code with status code object value

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const userRouter = require("./routes/users");
-const itemRouter = require("./routes/clothingItems")
+const itemRouter = require("./routes/clothingItems");
+const { ERROR_CODES } = require("./utils/errors");
 
 const { PORT = 3001 } = process.env;
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-
-
-app.use('/users', userRouter)
+app.use("/users", userRouter);
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '686acf37a6f4f0b72d2a50e4'
-  }
-  next()
-})
+    _id: "686acf37a6f4f0b72d2a50e4",
+  };
+  next();
+});
 
-app.use("/items", itemRouter)
+app.use("/items", itemRouter);
 app.use((req, res) => {
-  res.status(404).json({message: "Requested resource not found"})
-})
+  res.status(ERROR_CODES.NOT_FOUND).json({ message: "Requested resource not found" });
+});
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
