628c03d1bd418899f8e4fd383d37485e76af89f7
Donovan Jabbar <djabbar51@gmail.com>
2025-07-17 14:44:06 -0600

    style(routes/clothingItems): format file

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,17 +1,19 @@
 const router = require("express").Router();
-const { getAllItems, addNewItem, deleteItem, likeItem, unlikeItem } = require("../controllers/clothingItems")
-const {authenticateUser} = require("../middlewares/auth")
+const {
+  getAllItems,
+  addNewItem,
+  deleteItem,
+  likeItem,
+  unlikeItem,
+} = require("../controllers/clothingItems");
+const { authenticateUser } = require("../middlewares/auth");
 
-router.route("/")
-  .get(getAllItems)
-  .post(authenticateUser, addNewItem)
+router.use(authenticateUser);
+router.route("/").get(getAllItems).post(addNewItem);
 
-router.route('/:itemId')
-  .delete(authenticateUser, deleteItem)
+router.route("/:itemId").delete(deleteItem);
 
 // How would liking an item be a PUT and not a PATCH? We're not replacing the entire "item" resource in the database
 // We're only adding a reference to a user document to the likes array.
-router.route('/:itemId/likes')
-  .put(likeItem)
-  .delete(unlikeItem)
-module.exports = router
+router.route("/:itemId/likes").put(likeItem).delete(unlikeItem);
+module.exports = router;
