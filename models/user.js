ba460c4d968d24d82221bf7e13933d53f98bb616
Donovan Jabbar <djabbar51@gmail.com>
2025-07-17 14:06:43 -0600

    refactor: move statics property to options object

--- models/user.js
+++ models/user.js
@@ -1,53 +1,57 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: "You must enter a valid URL",
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      validate: {
+        validator: (v) => validator.isURL(v),
+        message: "You must enter a valid URL",
+      },
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: "You must enter a valid email address",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
     },
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    validate: {
-      validator: (v) => validator.isEmail(v),
-      message: "You must enter a valid email address",
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false
-  },
-  statics: {
-    async findUserByCredentials(email, password) {
-      const user = await this.findOne({ email })
-        .select("+password")
-        .orFail(() => {
-          const error = new Error("Invalid email address")
-          error.name = "UnauthorizedError";
-          throw error
-        });
+  {
+    statics: {
+      async findUserByCredentials(email, password) {
+        const user = await this.findOne({ email })
+          .select("+password")
+          .orFail(() => {
+            const error = new Error("Invalid email address");
+            error.name = "UnauthorizedError";
+            throw error;
+          });
 
-      const isMatched = await bcrypt.compare(password, user.password);
-      if (isMatched) return user;
+        const isMatched = await bcrypt.compare(password, user.password);
+        if (isMatched) return user;
 
-      const error = new Error("Invalid password");
-      error.name = "UnauthorizedError";
-      throw error;
+        const error = new Error("Invalid password");
+        error.name = "UnauthorizedError";
+        throw error;
+      },
     },
-  },
-});
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
